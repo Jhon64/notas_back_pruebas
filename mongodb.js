a0748d55751b9f6fdfee59e2286082f999374d6f
Jonathan Amaranto <jhtamaranto@gmail.com>
2021-11-05 15:34:59 -0500

    actualizacion de base de datos a host

--- mongodb.js
+++ mongodb.js
@@ -1,4 +1,4 @@
 const mongoose=require('mongoose')
-const conexion=mongoose.connect('mongodb://localhost:27017/notasdb')
+const conexion=mongoose.connect('mongodb+srv://jhon:sneider10@cluster0.nimo2.mongodb.net/notasdb?retryWrites=true&w=majority')
 
 module.exports=conexion
