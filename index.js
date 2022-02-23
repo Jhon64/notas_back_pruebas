02d53540e9e98a148c64631ad33f114930015f22
Jonathan Amaranto <41702621+Jhon64@users.noreply.github.com>
2022-02-22 19:07:05 -0500

    actualizando cors

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-
-const express=require('express')
-const morgan = require('morgan')
-const path =require('path')
-const indexRouter=require('./src/routes/index.api.router')
-const indexViewRouter=require('./src/routes/index.view.router')
-const apurataRouter=require('./src/routes/apurata.router')
-const bodyParser=require('body-parser')
-const App=express()
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const indexRouter = require("./src/routes/index.api.router");
+const indexViewRouter = require("./src/routes/index.view.router");
+const apurataRouter = require("./src/routes/apurata.router");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const App = express();
 //middlewares
-App.use(morgan('dev'))
+App.use(morgan("dev"));
 
-App.set('PORT',process.env.PORT||5500)
+App.set("PORT", process.env.PORT || 5500);
 
-App.set('views', path.join(__dirname ,'src/views'))
-App.set('view engine','ejs')
+App.set("views", path.join(__dirname, "src/views"));
+App.set("view engine", "ejs");
 
-App.use(bodyParser.urlencoded({extended:true}))
-App.use(bodyParser.json())
-App.use('/public',express.static(__dirname + '/public'))
+App.use(bodyParser.urlencoded({ extended: true }));
+App.use(bodyParser.json());
+App.use(cors());
+App.use("/public", express.static(__dirname + "/public"));
 
-App.use('/',indexViewRouter)
-App.use('/apurata',apurataRouter)
-App.use('/api/',indexRouter)
+App.use("/", indexViewRouter);
+App.use("/apurata", apurataRouter);
+App.use("/api/", indexRouter);
 
-App.listen(App.get('PORT'),()=>{
-    console.log('http://localhost:'+App.get('PORT'))
-})
+App.listen(App.get("PORT"), () => {
+  console.log("http://localhost:" + App.get("PORT"));
+});
